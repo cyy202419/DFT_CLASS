98c827b0ca89317edafb2b1f9ea6e08392eea427
cyy202419 <64142343+cyy202419@users.noreply.github.com>
2024-10-16 14:15:21 +0800

    Update hydrogen_LDA.py

--- hydrogen_LDA.py
+++ hydrogen_LDA.py
@@ -5,8 +5,7 @@ def rho(r):
     return (1/np.pi) * np.exp(-2 * r)
 
 def r_s(r):
-    # 限制r的值以避免溢出
-    r = np.minimum(r, 10)  # 你可以根据需要调整这个值
+    r = np.minimum(r, 10)
     return (3/4) ** (1/3) * np.exp(2*r/3)
 
 def epsilon_c(r):
@@ -19,8 +18,7 @@ def epsilon_c(r):
     beta2 = 6.1977
     beta3 = 3.3662
     beta4 = 0.62517
-    
-    # 确保对数函数的参数为正且不会太小
+  
     term = 1 + 1/(2*A * (beta1*x + beta2*x**2 + beta3*x**3 + beta4*x**4))
     if term <= 1:
         term = 1 + 1e-10  # 添加一个小的正数以避免对数为负或零
@@ -34,7 +32,7 @@ def integrand(r):
     if np.isfinite(ep) and np.isfinite(rho_val):
         return ep * rho_val * 4 * np.pi * r**2
     else:
-        return 0  # 如果epsilon_c或rho不是有限值，则返回0
+        return 0
 
 E_c, error = quad(integrand, 0, np.inf, epsabs=1e-10, epsrel=1e-10)
 
